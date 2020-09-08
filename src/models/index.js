9aeccb294be10b65896490fad59125181d497b68
신중빈 <jungbin4337@dgsw.hs.kr>
2020-09-08 13:38:09 +0900

    edit

--- src/models/index.js
+++ src/models/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('wiki', 'root', 'chlwlsdn', {
+const sequelize = new Sequelize('wiki', 'root', '1234', {
   host: 'localhost',
   dialect: 'mysql',
   logging: false,
--- src/router/auth/index.js
+++ src/router/auth/index.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const authCtrl = require('./auth.ctrl');
 
 router.post('/login', authCtrl.login);
+router.post('/register', authCtrl.register);
 
 module.exports = router;
